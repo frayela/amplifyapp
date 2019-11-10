cf4d7fff36f9b6745236b4e59dac7d9f19c24042
frayela <frayela@yahoo.com>
2019-11-10 18:12:38 +1300

    CLI commit

--- src/App.js
+++ src/App.js
@@ -7,9 +7,10 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
+        test
         <p>
           Edit <code>src/App.js</code> and save to reload.
-        </p>
+        </p>T-E-S-T
         <a
           className="App-link"
           href="https://reactjs.org"
